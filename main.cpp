d9b634886be4e1eaa7dfa09e8c6d108e6fdf9e83
Jacek Programista <jacek.torczynski.programista@gmail.com>
2023-01-17 13:56:04 +0100

    Created base class KsiazkaAdresowa and Uzytkownik

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "KsiazkaAdresowa.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    KsiazkaAdresowa ksiazkaAdresowa; //pierwszy obiekt
     return 0;
 }
